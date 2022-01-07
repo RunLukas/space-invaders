21d3390a9e9e646f797873ac22780bf65a4c6a5e
RunLukas <lukatrontelj092@gmail.com>
2022-01-07 23:09:01 +0100

    Added collision detection and 5 rows of enemies

--- game.js
+++ game.js
@@ -14,6 +14,15 @@ class Bullet{
 	}
 }
 
+class Enemy{
+	constructor(x, y){
+		this.x = x;
+		this.y = y;
+		this.enemies = new Image();
+		this.enemies.src = "enemy.png";
+	}
+}
+
 let canvas;
 let ctx;
 let buffer;
@@ -21,6 +30,12 @@ let buffer;
 let player;
 
 let bullets = [];
+
+let enemies = new Array(5).fill(0).map(() => new Array(11).fill(0));;
+
+let enemiesMovement = false;
+let enemiesCounterX = 0;
+let enemiesCounterY = 0;
 
 function initBackground(){
 	//set backround color of canvas to gray
@@ -72,18 +87,101 @@ function drawPlayer () {
 	buffer.drawImage(player.ship, player.x-20, player.y, 40, 20)
 }
 
+function detectCollision(i) {
+    for(j = 0; j < enemies.length; j++) {
+		for(let k = 0; k < enemies[j].length; k++){
+
+            if (bullets[i].x < enemies[j][k].x + 40 &&
+			bullets[i].x + 2 > enemies[j][k].x &&
+            bullets[i].y < enemies[j][k].y + 20 && 
+			bullets[i].y + 2 > enemies[j][k].y){
+				console.log(enemies[j].length)
+                enemies[j].splice(k, 1);
+				bullets.splice(i, 1);
+				//console.log(enemies[j].length)
+				return;
+			}
+		}
+    }
+}
+
 function updateBullets () {
 	//update all existing bullets
 	for(let i = 0; i < bullets.length; i++){
 		bullets[i].y -= 3;
 		
+		
+
 		//if bullet is off-screen then remove it from array 
 		if(bullets[i].y <= 0)
 			bullets.splice(i, 1);
+		else
+			detectCollision(i);
 
 		//if bullet hit an enemy remove both the bullet and the enemy
-		//detectCollision();
+		
 	}
+}
+function updateEnemies(){
+	if(enemies[0][enemies[0].length-1].x+20 > canvas.width){
+		enemiesMovement = true;
+		for(let i = 0; i < 5; i++){
+			for(let j = 0; j < 11; j++){
+				if(enemies[i][j] === undefined) break;
+				enemies[i][j].y += 5;
+			}
+		}
+	}
+	if(enemies[0][0].x-20 < 0){
+		enemiesMovement = false;
+		for(let i = 0; i < 5; i++){
+			for(let j = 0; j < 11; j++){
+				if(enemies[i][j] === undefined) break;
+				enemies[i][j].y += 5;
+			}
+		}
+	}
+	if(enemiesMovement == false){
+		for(let i = 0; i < 5; i++){
+			for(let j = 0; j < 11; j++){
+				if(enemies[i][j] === undefined) break;
+				enemies[i][j].x += 0.15;
+				
+			}
+		}
+	}
+	if(enemiesMovement == true){
+		for(let i = 0; i < 5; i++){
+			for(let j = 0; j < 11; j++){
+				if(enemies[i][j] === undefined) break;
+				enemies[i][j].x -= 0.15;
+			}
+		}
+	}
+}
+
+let isEmpty = true;
+
+function drawEnemies(){
+	//creates new enemies
+	if(isEmpty){
+		for(let i = 0; i < 5; i++){
+			for(let j = 0; j < 11; j++){
+				enemies[i][j] = new Enemy(43*j+30, i*35+50);
+				buffer.drawImage(enemies[i][j].enemies, enemies[i][j].x-20, enemies[i][j].y, 40, 20);
+				if(i == 4 && j == 10) isEmpty = false;
+			}
+		}
+	}
+
+	//draw all still existing enemies
+	for(let i = 0; i < 5; i++){
+		for(let j = 0; j < enemies[i].length; j++){
+			buffer.drawImage(enemies[i][j].enemies, enemies[i][j].x-20, enemies[i][j].y, 40, 20);
+		}
+	}
+	updateEnemies();
+	
 }
 
 function drawBullets () {
@@ -98,16 +196,17 @@ function drawBullets () {
 		buffer.stroke();
 	}
 
-	//update enemy
+	
 	updateBullets();
 }
 
 function draw () {
 	drawBackground();
-	drawPlayer();	
+	drawPlayer();
+	drawEnemies();	
 	drawBullets();
-	//drawEnemies();
 	window.requestAnimationFrame(draw);
+	
 }
 
 function init () {
